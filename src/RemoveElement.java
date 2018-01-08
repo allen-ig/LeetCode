16965abfad2f7185c801677db0f3d7d5771eb341
allen-ig <allen_ig@outlook.com>
2018-01-08 09:32:45 +0800

    RemoveElement

---
+++ src/RemoveElement.java
@@ -0,0 +1,12 @@
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length == 0) return 0;
+        int res = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] != val){
+                nums[res++] = nums[i];
+            }
+        }
+        return res;
+    }
+}
