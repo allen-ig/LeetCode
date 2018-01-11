9c179d766aab66e0cd11991ee9dca3c4c996028a
allen-ig <allen_ig@outlook.com>
2018-01-11 10:14:18 +0800

    ListNode

--- src/RemoveDuplicatesfromSortedList.java
+++ src/RemoveDuplicatesfromSortedList.java
@@ -1,16 +1,4 @@
 public class RemoveDuplicatesfromSortedList {
-    /**
-     * Definition for singly-linked list.
-     */
-    public class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode(int x) {
-            val = x;
-        }
-    }
-
     class Solution {
         public ListNode deleteDuplicates(ListNode head) {
             if (head == null || head.next == null) return head;
