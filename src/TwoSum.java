2953b66ddd03ee259890093dad8e510cc6d3f4c0
allen-ig <allen_ig@outlook.com>
2018-03-06 22:09:19 +0800

    Two Sum HashMap

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         int[] result=new int[2];
@@ -11,5 +13,21 @@ public class TwoSum {
             }
         }
         return result;
+    }
+
+    public int[] twoSum2(int[] nums, int target){
+        if (nums == null || nums.length < 2){
+            return new int[] {-1, -1};
+        }
+        int[] res = new int[] {-1, -1};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            if (map.containsKey(target - nums[i])){
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
+            }
+            map.put(nums[i], i);
+        }
+        return res;
     }
 }
