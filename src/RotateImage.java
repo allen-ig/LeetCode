5e1d43284eed975fa6a337069018fffae29f89a9
allen-ig <allen_ig@outlook.com>
2018-03-24 14:05:44 +0800

    Rotate Image transpose the matrix & flip horizontally

---
+++ src/RotateImage.java
@@ -0,0 +1,19 @@
+public class RotateImage {
+    public void rotate(int[][] matrix) {
+        if (matrix == null) return;
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = i; j < matrix[0].length;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix[0].length / 2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix[0].length - j - 1];
+                matrix[i][matrix[0].length - j - 1] = temp;
+            }
+        }
+    }
+}
