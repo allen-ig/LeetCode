a5cfd06df1790f1212e6d0be6cba95734924b8ee
allen-ig <allen_ig@outlook.com>
2018-03-30 10:00:56 +0800

    charAt() - '0'

---
+++ src/AddBinary.java
@@ -0,0 +1,19 @@
+/**
+ * sum(charAt() - '0') to calculate the result on each digit
+ * use % 2 and / 2 to carry bit
+ */
+public class AddBinary {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1, j = b.length() -1, carry = 0;
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        if (carry != 0) sb.append(carry);
+        return sb.reverse().toString();
+    }
+}
