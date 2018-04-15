277811c857f3d681a7bc54020f3d25488d9a52f8
allen-ig <allen_ig@outlook.com>
2018-04-15 13:54:12 +0800

    dynamic programming

---
+++ src/UniqueBinarySearchTree.java
@@ -0,0 +1,11 @@
+public class UniqueBinarySearchTree {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int maxCur = 0;
+        for (int i = 1; i < prices.length; i++){
+            max = Math.max(0, max += prices[i] - prices[i - 1]);
+            maxCur = Math.max(maxCur, max);
+        }
+        return maxCur;
+    }
+}
