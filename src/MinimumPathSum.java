ba94a4ace42612cd32cd48914b48d16cd62eb622
allen-ig <allen_ig@outlook.com>
2018-04-16 11:24:50 +0800

    dynamic programming

---
+++ src/MinimumPathSum.java
@@ -0,0 +1,14 @@
+public class MinimumPathSum {
+    public int minPathSum(int[][] grid) {
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid[0].length; j++){
+                if (i == 0 && j != 0) grid[i][j] += grid[i][j - 1];
+                if (i != 0 && j == 0) grid[i][j] += grid[i - 1][j];
+                if (i != 0 && j != 0){
+                    grid[i][j] += Math.min(grid[i][j - 1], grid[i - 1][j]);
+                }
+            }
+        }
+        return grid[grid.length - 1][grid[0].length - 1];
+    }
+}
