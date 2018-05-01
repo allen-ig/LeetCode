08867b13f9839b46d3f2dadd89e651b5d4605ffa
allen-ig <allen_ig@outlook.com>
2018-05-01 14:32:00 +0800

    bit manipulation do not use >> (arithmetic shift)

--- src/NumberOf1Bits.java
+++ src/NumberOf1Bits.java
@@ -1,9 +1,8 @@
 public class NumberOf1Bits {
     public int hammingWeight(int n){
         int count = 0;
-        int power2 = 1;
         while (n != 0){
-            count += n & power2;
+            count += n & 1;
             n >>>= 1;
         }
         return count;
