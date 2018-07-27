e27c721521790c5b79036f6be505336266f4873f
allen-ig <allen_ig@outlook.com>
2018-07-27 20:05:02 +0800

    bst iterative traversal

---
+++ src/BinarySearchTreePreorderTraversal.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinarySearchTreePreorderTraversal {
+    /**
+     * space: O(n)
+     * time: O(n)
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        while (root != null){
+            res.add(root.val);
+            if (root.right != null) stack.push(root.right);
+            root = root.left;
+            if (root == null && !stack.isEmpty()) root = stack.pop();
+        }
+        return res;
+    }
+}
