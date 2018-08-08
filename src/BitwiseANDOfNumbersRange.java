dea62982a239c4da83a91d68d317c235d22ee15a
allen-ig <allen_ig@outlook.com>
2018-08-08 09:26:54 +0800

    bit manipulation

---
+++ src/BitwiseANDOfNumbersRange.java
@@ -0,0 +1,12 @@
+public class BitwiseANDOfNumbersRange {
+    public int rangeBitwiseAnd(int m, int n) {
+        if (m == 0) return 0;
+        int res = 1;
+        while (m != n) {
+            m >>= 1;
+            n >>= 1;
+            res <<= 1;
+        }
+        return m * res;
+    }
+}
