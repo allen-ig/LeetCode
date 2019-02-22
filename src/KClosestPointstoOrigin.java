bc44106ae33c52075a21e7f9d29e6757c012e2ea
allen <allen_ig@outlook.com>
2019-02-22 14:13:16 -0500

    Priority Queue

---
+++ src/KClosestPointstoOrigin.java
@@ -0,0 +1,14 @@
+import java.util.PriorityQueue;
+
+public class KClosestPointstoOrigin {
+    public int[][] kClosest(int[][] points, int K) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
+        for (int[] p : points){
+            pq.offer(p);
+            if (pq.size() > K) pq.poll();
+        }
+        int[][] res = new int[K][2];
+        while (K > 0) res[--K] = pq.poll();
+        return res;
+    }
+}
